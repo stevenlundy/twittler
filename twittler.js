77872df634ca3be4bd48e119bf2a9821bdc20070
Steven Lundy <steven.lundy@gmail.com>
2015-07-12 08:57:40 -0400

    Added User constructor and userInfo object with sample data

--- twittler.js
+++ twittler.js
@@ -1,3 +1,15 @@
+var User = function(name, handle, picture){
+  this.name = name;
+  this.handle = handle;
+  this.picture = picture;
+};
+
+var userInfo = {};
+userInfo.shawndrost = new User('Shawn Drost', 'shawndrost', 'https://pbs.twimg.com/profile_images/586651979348250624/3n3kd_5P_400x400.jpg');
+userInfo.sharksforcheap = new User('Anthony Phillips', 'sharksforcheap', 'https://pbs.twimg.com/profile_images/3737419103/b81bf3e4ba350fec48493784880241c2_400x400.jpeg');
+userInfo.mracus = new User('Marcus Phillips', 'mracus', 'https://pbs.twimg.com/profile_images/460312331923107840/m1Fip-Vt_400x400.jpeg');
+userInfo.douglascalhoun = new User('Douglas Calhoun', 'douglascalhoun', 'https://pbs.twimg.com/profile_images/1831644430/DSC02750_400x400.JPG');
+
 $(document).ready(function(){
   var $body = $('body');
   $body.html('');
